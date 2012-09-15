3603cd3111d58996d1efa907b797e8deb715fdb7
Aayush Upadhyay <aayushu@gmail.com>
2012-09-15 00:49:00 -0400

    added html, changed handler

--- tornado/main.py
+++ tornado/main.py
@@ -12,11 +12,16 @@ import tornado.web
 import pymongo
 from bson import json_util
 
+#Foursquare
+import foursquare
+
 class Application(tornado.web.Application):
 
     def __init__(self):
         handlers = [
             (r"/", HomeHandler),
+            (r"/main", MainHandler),
+            (r"/FoursquareHandler", FoursquareHandler),
             ]
 
         settings = dict(
@@ -29,10 +34,38 @@ class Application(tornado.web.Application):
 class BaseHandler(tornado.web.RequestHandler):
     pass
 
+class FoursquareHandler(BaseHandler):
+    def post(self):
+        client = foursquare.Foursquare(client_id='QYEIVBMULP11CPVHP4MSHXDB2VIZ12LDDUTMMJL2YSP2IJJA', 
+                               client_secret='L04TIELKXWIHKVXWI1PRENGM1YFSPHHX0PEUZQSUIMDVHDDU', 
+                               redirect_uri='http://localhost:3000/main')
+
+        # Build the authorization url for your app
+        auth_uri = client.oauth.auth_url()        
+        self.write('Hello World, redirecting to %s' % auth_uri)
+        self.redirect(auth_uri)
+
+class MainHandler(BaseHandler):
+    def get(self):
+
+        client = foursquare.Foursquare(client_id='QYEIVBMULP11CPVHP4MSHXDB2VIZ12LDDUTMMJL2YSP2IJJA', 
+                               client_secret='L04TIELKXWIHKVXWI1PRENGM1YFSPHHX0PEUZQSUIMDVHDDU', 
+                               redirect_uri='http://localhost:3000/main')
+        # Interrogate foursquare's servers to get the user's access_token
+        access_token = client.oauth.get_token(self.get_argument("code"))
+
+        # Apply the returned access token to the client
+        client.set_access_token(access_token)
+
+        # Get the user's data
+        user = client.users()
+        self.write("Authenticated alright!")
+        self.write(user)
+
 
 class HomeHandler(BaseHandler):
     def get(self):
-        self.write('Hello World!')
+        self.render("home.html");
 
 
 def main(port='3000', address='127.0.0.1'):
