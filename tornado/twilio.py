5be0bea538cb38d8420b88557db7c8d0956fe20f
Aayush Upadhyay <aayushu@gmail.com>
2012-09-16 06:04:05 -0400

    twilio stuff

--- tornado/twilio.py
+++
@@ -1,8 +0,0 @@
-from twilio.rest import TwilioRestClient
-
-account_sid = ""
-auth_token = ""
-client = TwilioRestClient(account_sid, auth_token)
-
-message = client.sms.messages.create(to="1234345", from="23432423423", body="hi thurr")
-
